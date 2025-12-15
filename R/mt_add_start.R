15d2a81763ba1e29c7865d7196d8fd16c5bc6af0
Edward Hurme <edwardhurme@gmail.com>
2025-12-15 11:12:37 +0100

    Update mt_add_start.R

--- R/mt_add_start.R
+++ R/mt_add_start.R
@@ -3,12 +3,13 @@
 mt_add_start <- function(m2){
   require(move2)
   require(magrittr)
+  require(dplyr)
   track_data <- mt_track_data(m2)
 
   captures <- mt_as_move2(x = track_data %>%
                             mutate(timestamp = deploy_on_timestamp,
                                    geometry = deploy_on_location) %>%
-                            select(individual_local_identifier, timestamp, geometry),
+                            dplyr::select(individual_local_identifier, timestamp, geometry),
                           time_column = "timestamp",
                           track_id_column = "individual_local_identifier", sf_column_name = "geometry")
   captures <- move2::mt_set_track_data(captures, data = track_data)
@@ -16,6 +17,8 @@ mt_add_start <- function(m2){
   m2 <- mt_stack(m2,
                       captures,
                       .track_combine = "merge")
+  m2 <- m2[order(m2$timestamp),]
+  m2 <- m2[order(mt_track_id(m2)),]
   return(m2)
 }
 
